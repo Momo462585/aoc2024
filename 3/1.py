95f0aa6ded49b3821dc446bc0fcdd36e895e263a
Monika Gong <monika.gong@lhind.dlh.de>
2024-12-17 12:52:18 +0100

    änderung

--- 3/1.py
+++ 3/1.py
@@ -7,7 +7,9 @@ list = re.findall(r"mul\((\d+),(\d+)\)", line)
 
 sum = 0
 mul = 0
+
 for element in list:
-    mul = int(element[0]) * int(element[1])
+    mul = int(element[0]) * int(element[1]) # tuple only read
     sum += mul
+
 print(sum)
---
+++ 3/2.py
@@ -0,0 +1,20 @@
+import re
+
+with open('input.txt','r') as datei:
+    line = datei.read().strip()
+
+list = re.search(r"mul\((\d+),(\d+)\)", line)
+
+valied = re.search(r"do\(\)(.*?)don't\(\)",line,re.DOTALL)
+
+for i in valied:
+    list = re.findall(r"mul\((\d+),(\d+)\)", line)
+
+sum = 0
+mul = 0
+
+for element in list:
+    mul = int(element[0]) * int(element[1]) # tuple only read
+    sum += mul
+
+print(sum)
